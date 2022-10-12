b3463f2ab24bfaacdac52063d2278d510c1ad830
analogdev.eth <dejijolaoluwa@gmail.com>
2022-10-12 08:58:41 +0100

    Add function to calculate price of bin in terms of tokenY

---
+++ dexcandles-v2/src/utils/pricing.ts
@@ -0,0 +1,44 @@
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts";
+import { Token } from "../../generated/schema";
+
+/**
+ * Returns the price of the bin given its id and bin step
+ * (1 + binStep / 10_000) ** (id - 8388608)
+ *
+ * @param { BigInt } id
+ * @param { BigInt } binStep
+ */
+export function getPriceYOfBin(
+  binId: BigInt,
+  binStep: BigInt,
+  tokenX: Token,
+  tokenY: Token
+): BigDecimal {
+  const BASIS_POINT_MAX = new BigDecimal(BigInt.fromI32(10_000));
+  const BIN_STEP = new BigDecimal(binStep);
+  const REAL_SHIFT = 8388608;
+  const BIG_DECIMAL_ONE = BigDecimal.fromString("1");
+
+  // compute bpVal = (1 + binStep / 10_000)
+  const bpVal = BIG_DECIMAL_ONE.plus(BIN_STEP.div(BASIS_POINT_MAX));
+
+  // compute bpVal ** (id - 8388608)
+  const loop = binId.toI32() - REAL_SHIFT;
+  const isPositive = loop > 0;
+
+  let result = BIG_DECIMAL_ONE;
+
+  for (let i = 0; i < Math.abs(loop); i++) {
+    if (isPositive) {
+      result = result.times(bpVal);
+    } else {
+      result = result.div(bpVal);
+    }
+  }
+
+  // get price in terms of tokenY
+  const tokenYDecimals = BigDecimal.fromString(`1e${tokenY.decimals.toI32()}`);
+  const tokenXDecimals = BigDecimal.fromString(`1e${tokenX.decimals.toI32()}`);
+
+  return result.times(tokenXDecimals).div(tokenYDecimals);
+}
