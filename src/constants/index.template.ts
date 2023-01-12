d357cee5fa79ea694544d0157fa1175f0711c023
gaepsuni <105390436+gaepsuni@users.noreply.github.com>
2023-01-12 13:28:08 -0800

    Set up mustache (#27)
    * set up mustache
    
    * set up mustache
    
    * update readme
    
    * clean up todo comment

--- src/constants/index.template.ts
+++ src/constants/index.template.ts
@@ -17,8 +17,8 @@ export const BIG_INT_ZERO = BigInt.fromI32(0);
 export const NULL_CALL_RESULT_VALUE =
   "0x0000000000000000000000000000000000000000000000000000000000000001";
 
-export const LBFACTORY_ADDRESS = Address.fromString('0xC8Af41e49e2C03eA14706C7aa9cEE60454bc5c03');
+export const LBFACTORY_ADDRESS = Address.fromString('{{ lbfactory_address }}');
 
-export const JOE_DEX_LENS_ADDRESS = Address.fromString('0x1Be66E6aC1A92f84F6D39E8ED3fc2E0AF8D05747');
+export const JOE_DEX_LENS_ADDRESS = Address.fromString('{{ joe_dex_lens_address }}');
 
-export const WAVAX_ADDRESS = Address.fromString('0xaE4EC9901c3076D0DdBe76A520F9E90a6227aCB7');
+export const WAVAX_ADDRESS = Address.fromString('{{ wavax_address }}');
