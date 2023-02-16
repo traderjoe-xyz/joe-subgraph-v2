6443ff1a1afa7907c264e879d8ddb015ba3f3d4c
Maxime <52084503+Max-3-7@users.noreply.github.com>
2023-02-16 09:49:59 +0100

    handle getFeeRecipient reverted

--- src/entities/vaultFactory.ts
+++ src/entities/vaultFactory.ts
@@ -1,6 +1,7 @@
 import { Address } from "@graphprotocol/graph-ts";
 import { VaultFactory } from "../../generated/schema";
 import {
+  ADDRESS_ZERO,
   BIG_DECIMAL_ZERO,
   VAULT_FACTORY_ADDRESS,
   BIG_INT_ONE,
@@ -11,13 +12,16 @@ export function loadVaultFactory(
   id: Address = VAULT_FACTORY_ADDRESS
 ): VaultFactory {
   let vaultFactory = VaultFactory.load(id.toHexString());
-  const vaultFactoryContract = VaultFactoryABI.bind(id);
 
   if (!vaultFactory) {
+    const vaultFactoryContract = VaultFactoryABI.bind(id);
     vaultFactory = new VaultFactory(id.toHexString());
-    vaultFactory.feeRecipient = vaultFactoryContract
-      .try_getFeeRecipient()
-      .value.toHexString();
+    const feeRecipient = vaultFactoryContract.try_getFeeRecipient();
+    if (feeRecipient.reverted) {
+      vaultFactory.feeRecipient = ADDRESS_ZERO.toHexString();
+    } else {
+      vaultFactory.feeRecipient = feeRecipient.value.toHexString();
+    }
     vaultFactory.vaultCount = BIG_INT_ONE;
     vaultFactory.strategyCount = BIG_INT_ONE;
     vaultFactory.totalValueLockedUSD = BIG_DECIMAL_ZERO;
